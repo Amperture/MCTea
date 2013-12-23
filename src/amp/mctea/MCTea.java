6d1b0717671cfac954083b131c52f4d882aac0cf
Alex Best <theamp@gmail.com>
2013-12-23 02:34:38 -0600

    We begin...

---
+++ src/amp/mctea/MCTea.java
@@ -0,0 +1,9 @@
+package amp.mctea;
+
+import amp.mctea.lib.Reference;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class MCTea {
+
+}
---
+++ src/amp/mctea/lib/Reference.java
@@ -0,0 +1,9 @@
+package amp.mctea.lib;
+
+public class Reference {
+	
+	public static final String MOD_ID = "amp.mctea";
+	public static final String MOD_NAME = "MCTea";
+	public static final String VERSION = "0.0.1";
+	
+}
