8daf69a54eaa78831c2d2a532cf7865a92050805
Alex Best <theamp@gmail.com>
2013-12-23 02:57:19 -0600

    Cleanup

--- src/amp/mctea/MCTea.java
+++ src/amp/mctea/MCTea.java
@@ -2,8 +2,14 @@ package amp.mctea;
 
 import amp.mctea.lib.Reference;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class MCTea {
-
+	
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event){
+			
+	}
 }
